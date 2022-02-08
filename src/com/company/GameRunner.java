9cd9cd970eb69f0e4532beeafb9fa5cbcbe7d3a9
omer <11omerzz1@gmail.com>
2022-02-08 13:51:49 +0200

    start

---
+++ src/com/company/GameRunner.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class GameRunner {
+    Boardgame bg;
+//    public GameRunner()
+//    {
+//        this.bg = new bg(...);
+//    }
+
+    public void root()
+    {
+        // DB TOP 5 LIST OF GAMES (NAMES)
+        // IO NAME OF GAME
+        this.bg = new bg(...); //
+    }
+
+    public void run()
+    {
+        bg.initialize(); //*
+        while(!isGameOver){
+            p = getNextPlayer();
+            p.makeMove();
+        }
+    }
+
+}
