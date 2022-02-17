66b260b095c3101d04b96019880709b81a43be37
Raphael <raphael.abenmoha@live.biu.ac.il>
2022-02-17 18:24:50 +0200

    Pawn is legal move added

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,8 +1,9 @@
 package com.company;
 
 public class Board {
-    private Piece[][] board; // null = empty /
+    private Piece[][] board; // null = empty
     public Board(Piece[][] board){this.board = board;}
+
     public Piece[][] getState(){return this.board;}
 
 }
--- src/com/company/Pawn.java
+++ src/com/company/Pawn.java
@@ -1,7 +1,44 @@
 package com.company;
 
 public class Pawn extends Piece{
-    boolean isFirstMove = true;
+    private boolean isFirstMove = true;
+
+    public Pawn(String color){
+        this.color = color;
+    }
+    public boolean isLegalMove(Locations loc, Board state){
+        Location src = loc.getSrc();
+        Location dst = loc.getDst();
+
+        int i = src.getX();
+        int j = src.getY();
+        int x = src.getX() - dst.getX();
+        int y = src.getY() - dst.getY();
+
+        if (this.isFirstMove){
+            this.isFirstMove = false;
+             if ((Math.abs(x) == 1 || Math.abs(x) == 2) && Math.abs(y) == 0 && state.getState()[dst.getX()][dst.getY()] == null){
+                 return true;
+             }
+        }
+
+        // Check if movement of pawn is in the correct direction
+        if (this.color.equals("WHITE") && x < 0){
+            return false;
+        }
+        else if (this.color.equals("BLACK") && x > 0){
+            return false;
+        }
+        // Check the move is only one cell front and zero or one cell in diagonal
+        if (Math.abs(x) != 1 || Math.abs(y) > 1){
+            return false;
+        }
+        // If moving in diagonal check that there is an enemy piece at destination place
+        if(Math.abs(y) == 1 && this.color.equals(state.getState()[dst.getX()][dst.getY()].getColor())){
+            return false;
+        }
+        return true;
+    }
     //todo: if it reached to the other side should be replaced with another piece
 
 }
