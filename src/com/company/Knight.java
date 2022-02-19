23032c3160223c1e4ab55693543bea4e6d7fc15b
Raphael <raphael.abenmoha@live.biu.ac.il>
2022-02-19 20:36:42 +0200

    Knight is legal move added

--- src/com/company/Knight.java
+++ src/com/company/Knight.java
@@ -1,4 +1,26 @@
 package com.company;
 
-public class Knight extends Piece{
+public class Knight extends Piece {
+
+    public Knight(String color) {
+        this.color = color;
+    }
+
+    public boolean isLegalMove(Locations loc, Board state) {
+        Location src = loc.getSrc();
+        Location dst = loc.getDst();
+
+        int i = src.getX();
+        int j = src.getY();
+        int x = src.getX() - dst.getX();
+        int y = src.getY() - dst.getY();
+
+        if (Math.abs(x) == 1 && Math.abs(y) == 2) {
+            return true;
+        }
+        if (Math.abs(x) == 2 && Math.abs(y) == 1) {
+            return true;
+        }
+        return false;
+    }
 }
